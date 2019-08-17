f0651ccc0b14707c21d38afa11d7dc5a53a8a041
rgdekoning <rgdekoning@gmail.com>
2019-08-17 09:22:16 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# js-timeline
-Created with CodeSandbox
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// import 'babel-polyfill';
+import Timeline from './modules/Timeline';
+
+Timeline.init();
+
+document.getElementById('app').innerHTML = `
+<h1>Placeholder</h1>
+`;
